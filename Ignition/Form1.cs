157a6a86062e7d3433deabddc788753681442b79
Dylan Patton <patton_ist07@hotmail.com>
2017-09-26 14:59:55 +1000

    initial stuff

--- Ignition/Form1.cs
+++ Ignition/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Ignition
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = Program.APPLICATION_NAME;
         }
     }
 }
--- Ignition/Program.cs
+++ Ignition/Program.cs
@@ -6,17 +6,22 @@ using System.Windows.Forms;
 
 namespace Ignition
 {
+    
+
     static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+
+        public static string APPLICATION_NAME = "Ignition";
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
