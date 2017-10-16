6969bb8ee631997d31688ddc7df44b9cbeda510d
Dylan Patton <patton_ist07@hotmail.com>
2017-10-16 12:26:28 +1100

    Program completed (mostly)

--- Ignition/Form1.Designer.cs
+++ Ignition/Form1.Designer.cs
@@ -28,21 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.tbClientName = new System.Windows.Forms.TextBox();
+            this.tbPurpose = new System.Windows.Forms.TextBox();
+            this.pbRandom = new System.Windows.Forms.ProgressBar();
+            this.lblClientName = new System.Windows.Forms.Label();
+            this.lblPurpose = new System.Windows.Forms.Label();
+            this.lblDone = new System.Windows.Forms.Label();
+            this.tmrPB = new System.Windows.Forms.Timer(this.components);
+            this.btnTest = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(313, 245);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(121, 23);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Create Folders";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // tbClientName
+            // 
+            this.tbClientName.Location = new System.Drawing.Point(58, 64);
+            this.tbClientName.Name = "tbClientName";
+            this.tbClientName.Size = new System.Drawing.Size(376, 22);
+            this.tbClientName.TabIndex = 1;
+            // 
+            // tbPurpose
+            // 
+            this.tbPurpose.Location = new System.Drawing.Point(58, 142);
+            this.tbPurpose.Name = "tbPurpose";
+            this.tbPurpose.Size = new System.Drawing.Size(376, 22);
+            this.tbPurpose.TabIndex = 2;
+            // 
+            // pbRandom
+            // 
+            this.pbRandom.Location = new System.Drawing.Point(58, 203);
+            this.pbRandom.Name = "pbRandom";
+            this.pbRandom.Size = new System.Drawing.Size(376, 23);
+            this.pbRandom.Step = 1;
+            this.pbRandom.TabIndex = 3;
+            // 
+            // lblClientName
+            // 
+            this.lblClientName.AutoSize = true;
+            this.lblClientName.Location = new System.Drawing.Point(55, 44);
+            this.lblClientName.Name = "lblClientName";
+            this.lblClientName.Size = new System.Drawing.Size(101, 17);
+            this.lblClientName.TabIndex = 4;
+            this.lblClientName.Text = "Client Name(s)";
+            // 
+            // lblPurpose
+            // 
+            this.lblPurpose.AutoSize = true;
+            this.lblPurpose.Location = new System.Drawing.Point(55, 122);
+            this.lblPurpose.Name = "lblPurpose";
+            this.lblPurpose.Size = new System.Drawing.Size(113, 17);
+            this.lblPurpose.TabIndex = 5;
+            this.lblPurpose.Text = "Purpose for loan";
+            // 
+            // lblDone
+            // 
+            this.lblDone.AutoSize = true;
+            this.lblDone.Location = new System.Drawing.Point(58, 233);
+            this.lblDone.Name = "lblDone";
+            this.lblDone.Size = new System.Drawing.Size(45, 17);
+            this.lblDone.TabIndex = 6;
+            this.lblDone.Text = "Done!";
+            this.lblDone.Visible = false;
+            // 
+            // tmrPB
+            // 
+            this.tmrPB.Tick += new System.EventHandler(this.tmrPB_Tick);
+            // 
+            // btnTest
+            // 
+            this.btnTest.Enabled = false;
+            this.btnTest.Location = new System.Drawing.Point(202, 262);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 7;
+            this.btnTest.Text = "test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Visible = false;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(486, 297);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.lblDone);
+            this.Controls.Add(this.lblPurpose);
+            this.Controls.Add(this.lblClientName);
+            this.Controls.Add(this.pbRandom);
+            this.Controls.Add(this.tbPurpose);
+            this.Controls.Add(this.tbClientName);
+            this.Controls.Add(this.btnCreate);
             this.Name = "Main";
             this.Text = "name";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox tbClientName;
+        private System.Windows.Forms.TextBox tbPurpose;
+        private System.Windows.Forms.ProgressBar pbRandom;
+        private System.Windows.Forms.Label lblClientName;
+        private System.Windows.Forms.Label lblPurpose;
+        private System.Windows.Forms.Label lblDone;
+        private System.Windows.Forms.Timer tmrPB;
+        private System.Windows.Forms.Button btnTest;
     }
 }
 
--- Ignition/Form1.cs
+++ Ignition/Form1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Ignition
 {
     public partial class Main : Form
     {
+        int count = 0;
+        bool load = true;
         public Main()
         {
             InitializeComponent();
@@ -20,6 +23,97 @@ namespace Ignition
         private void Form1_Load(object sender, EventArgs e)
         {
             this.Text = Program.APPLICATION_NAME;
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            string userPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string year = DateTime.Now.Year.ToString();
+            string fileDirectory = Directory.GetCurrentDirectory() + "\\Files\\";
+
+            string[] fileList = Directory.GetFiles(fileDirectory, "*.*");
+
+            
+
+            if (tbClientName.Text == "" || tbPurpose.Text == "")
+            {
+                MessageBox.Show("Please enter text into the Text boxes.");
+                return;
+            }
+            string newDirectory = userPath + "\\Google Drive\\ClientFolder\\"
+                + tbClientName.Text + "\\" + year + " " + tbPurpose.Text + "\\";
+
+            if (Directory.Exists(newDirectory))
+            {
+                MessageBox.Show("That Profile exists already.");
+                return;
+            }
+
+            
+
+            Directory.CreateDirectory(newDirectory);
+            Directory.CreateDirectory(newDirectory + "Client Profile\\");
+            Directory.CreateDirectory(newDirectory + "Emails\\");
+
+            foreach (string file in fileList)
+            {
+                string fName = file.Substring(fileDirectory.Length);
+                try
+                {
+                    // Will not overwrite if the destination file already exists.
+                    File.Copy(Path.Combine(fileDirectory, fName), Path.Combine(newDirectory, fName));
+                }
+                catch (IOException copyError)
+                {
+                    MessageBox.Show(copyError.Message);
+                    return;
+                }
+            }
+
+            tmrPB.Interval = new Random().Next(1, 6);
+            tmrPB.Enabled = true;
+        }
+
+        private void tmrPB_Tick(object sender, EventArgs e)
+        {
+            if (load)
+            {
+                pbRandom.Increment(1);
+                if (pbRandom.Value == pbRandom.Maximum)
+                {
+                    load = false;
+                    count = 200;
+                    lblDone.Visible = true;
+                    tmrPB.Interval = 1;
+                }
+            }
+            else
+            {
+
+                count--;
+                if (count == 0)
+                {
+                    pbRandom.Value = 0;
+                    pbRandom.Update();
+                    lblDone.Visible = false;
+                    load = true;
+                    tmrPB.Enabled = false;
+                }
+            }
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            string fileDirectory = Directory.GetCurrentDirectory() + "\\Files\\";
+            string[] fileList = Directory.GetFiles(fileDirectory, "*.*");
+            foreach (string file in fileList)
+            {
+                string fName = file.Substring(fileDirectory.Length);
+                lblClientName.Text = fName;
+                
+            }
+            
+            
         }
     }
 }
